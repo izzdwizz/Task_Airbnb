b8480347a2f2d7474e2c12c971b6221745237f4c
Izuchukwu Uchendu <izuchukwu.uchendu@gmail.com>
2023-07-05 16:34:43 +0100

    First commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import classes from './style.module.css';
+import logo from '../../assets/logo/long-logo.png';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import LanguageIcon from '@mui/icons-material/Language';
+import ProfileMenu from './ProfileMenu';
+
+function Header() {
+	return (
+		<div className={classes.navbar}>
+			<img src={logo} alt='logo' className={classes.navbar_logo} />
+			<div className={classes.searchBar}>
+				<div className={classes.searchbartext}>Any Where</div>
+				<div className={classes.searchbartext}>Any Week</div>
+				<div className={classes.searchbartext2}>
+					Add Guests <span></span>
+				</div>
+				<div className={classes.searchIcondiv}>
+					<SearchRoundedIcon className={classes.searchIcon} />
+				</div>
+			</div>
+
+			<div className={classes.airprofile}>
+				<div className={classes.airhome}>Airbnb your home</div>
+				<div className={classes.airglobe}>
+					{' '}
+					<LanguageIcon sx={{ fontSize: '1.3rem' }} />
+				</div>
+				<div className={classes.airprofile}>
+					<ProfileMenu />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Header;
